c2f34ed19c65533dfa6cfb96a6c4958f9421a122
Krmsh1n5 <Dima7715@icloud.com>
2025-05-20 00:56:59 +0400

    Improved World class

--- world/World.java
+++ world/World.java
@@ -2,16 +2,21 @@ package world;
 
 import java.util.Arrays;
 import items.Item;
+import items.Chest;
 import entities.Entity;
 
 public class World {
-    private Entity[] entities = new Entity[0];
-    private Room[] rooms = new Room[0];
-    private Item[] items = new Item[0];
+    private final Entity[] entities;
+    private final Room[] rooms;
+    private final Door[] doors;
+    private final Chest[] chests;
+    private final Item[] items;
 
-    public World(Entity[] entities, Room[] rooms, Item[] items) {
+    public World(Entity[] entities, Room[] rooms, Door[] doors, Chest[] chest, Item[] items) {
         this.entities = entities;
         this.rooms = rooms;
+        this.doors = doors;
+        this.chests = chest;
         this.items = items;
     }
 
@@ -22,51 +27,25 @@ public class World {
     public Room[] getRooms() {
         return rooms;
     }
+    public Door[] getDoors() {
+        return doors;
+    }
+    public Chest[] getChests() {
+        return chests;
+    }
     public Item[] getItems() {
         return items;
     }
     // Setters
-    public void setEntities(Entity[] entities) {
-        this.entities = entities;
-    }
-    public void setRooms(Room[] rooms) {
-        this.rooms = rooms;
-    }
-    public void setItems(Item[] items) {
-        this.items = items;
-    }
+    // public void setEntities(Entity[] entities) {
+    //     this.entities = entities;
+    // }
+    // public void setRooms(Room[] rooms) {
+    //     this.rooms = rooms;
+    // }
+    // public void setItems(Item[] items) {
+    //     this.items = items;
+    // }
 
     // Methods
-    public void addEntity(Entity entity) {
-        entities = Arrays.copyOf(entities, entities.length + 1);
-        entities[entities.length - 1] = entity;
-    }
-
-    public void addRoom(Room room) {
-        rooms = Arrays.copyOf(rooms, rooms.length + 1);
-        rooms[rooms.length - 1] = room;
-    }
-
-    public void addItem(Item item) {
-        items = Arrays.copyOf(items, items.length + 1);
-        items[items.length - 1] = item;
-    }
-
-    public void removeEntity(Entity entity) {
-        entities = Arrays.stream(entities)
-                .filter(e -> !e.equals(entity))
-                .toArray(Entity[]::new);
-    }
-
-    public void removeRoom(Room room) {
-        rooms = Arrays.stream(rooms)
-                .filter(r -> !r.equals(room))
-                .toArray(Room[]::new);
-    }
-
-    public void removeItem(Item item) {
-        items = Arrays.stream(items)
-                .filter(i -> !i.equals(item))
-                .toArray(Item[]::new);
-    }
 }
